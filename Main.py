7b735357169496e8b7e80c3bc4e0b2a814ae06d3
chughes271 <chhughes@mtu.edu>
2024-12-02 14:41:54 -0500

    attempted to change the strucutre of an interface. TTT V3. Most logic (winning, tie etc.) is the same

--- Main.py
+++ Main.py
@@ -1,4 +1,207 @@
 import re
+import abc
+
+class Game(abc.ABC):
+
+    @abc.abstractmethod
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def set_move_on_board(self,move,name):
+        pass
+
+    #unused
+    @abc.abstractmethod
+    def get_board(self):
+        pass
+
+    @abc.abstractmethod
+    def display_board(self):
+        pass
+
+    @abc.abstractmethod
+    def valid_inputs(self,playerinputs):
+        pass
+
+    @abc.abstractmethod
+    def spot_is_playable(self,playerinputs):
+        pass
+
+    #unused
+    @abc.abstractmethod
+    def get_valid_inputs(self):
+        pass
+
+    @abc.abstractmethod
+    def game_is_won(self,name):
+        pass
+
+    @abc.abstractmethod
+    def check_pattern(self, length):
+        pass
+
+    @abc.abstractmethod
+    def search_board(self, row, col, xdir, ydir, length):
+        pass
+
+    @abc.abstractmethod
+    def game_is_tie(self):
+        pass
+
+
+class tictactoe(Game):
+
+    def __init__(self):
+        self.__board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
+        self.__validinputs = ["0","1","2"]
+        self.__winlength = 3
+
+
+    def set_move_on_board(self,playermove,name):
+        x = int(playermove[0])
+        y = int(playermove[1])
+
+        self.__board[x][y] = name
+
+    def get_board(self):
+        return self.__board
+
+    def display_board(self):
+        for row in self.__board:
+            print(row)
+
+    def valid_inputs(self,playerinputs):
+        if len(playerinputs) != 2:
+            return False
+
+        for pi in playerinputs:
+            if pi not in self.__validinputs:
+                #print("not valid")
+                return False
+        #print("valid")
+        return True
+
+    def spot_is_playable(self,playerinputs):
+        x = int(playerinputs[0])
+        y = int(playerinputs[1])
+
+        if self.__board[x][y] != "":
+            #print("not playable")
+            return False
+        #print("playable")
+        return True
+
+
+    def get_valid_inputs(self):
+        return self.__validinputs
+
+
+    def game_is_won(self,playername):
+        found, winner = self.check_pattern(self.__winlength)
+        if found:
+            self.display_board()
+            print("Winner: " +winner)
+            return True
+
+
+    def check_pattern(self, length):
+
+        rows = len(self.__board)
+        cols = len(self.__board[0])
+
+        #             right,   down, right+down, left+down
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+
+        for row in range(rows):
+            for col in range(cols):
+                for xdir, ydir in directions:
+                    if self.search_board(row, col, xdir, ydir, length):
+                        return True, self.__board[row][col]
+
+        return False, None
+
+
+    def search_board(self, row, col, xdir, ydir, length):
+
+        lookupvalue = self.__board[row][col]
+        if lookupvalue == "":
+            return False
+
+        for i in range(length):
+            r = row + i * xdir
+            c = col + i * ydir
+            # Check bounds and value
+            if not (0 <= r < len(self.__board) and 0 <= c < len(self.__board[0]) and self.__board[r][c] == lookupvalue):
+                return False
+
+        return True
+
+
+    def game_is_tie(self):
+        for row in self.__board:
+            if "" in row:
+                return False
+
+        self.display_board()
+        print("game is a tie")
+        return True
+
+
+
+class Player():
+
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def get_move(self):
+        pinput = input(self.__name + ", please play your move")
+
+        return re.findall(r'[0-9]+',pinput)
+
+
+    #Unsure where this game_in_progress should go.
+    #Part of me wants it in the game object because the game is responsible for doing game things (the game flow)....
+    #BUT the game doesn't know anything about a player object so I know this isn't correct.
+
+def game_in_progress(g,players):
+    for p in players:
+        g.display_board()
+        pname = p.get_name()
+
+        move = p.get_move()
+        while not g.valid_inputs(move) or not g.spot_is_playable(move):
+            print("Inputs not valid, try again")
+            move = p.get_move()
+        g.set_move_on_board(move,pname)
+        if g.game_is_won(pname) or g.game_is_tie():
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    ttt = tictactoe()
+    players = [Player("Cody"), Player("Troy")]
+
+    #please don't judge me too hard for this. I am unsure how to nicely facilitate the game.
+    while game_in_progress(ttt, players):
+        pass
+
+
+
+
+
+
+
+
+"""import re
 
 ## Comments:
 ## - You have a bunch of unused variables in method calls
@@ -31,11 +234,6 @@ class Game():
             ["", "", ""],
             ["", "", ""]
         ]
-        """        self.__board = [
-            ["Troy", "Cody", ""],
-            ["Cody", "Troy", "Troy"],
-            ["Cody", "Troy", "Cody"]
-        ]"""
 
     def get_board(self):
         return self.__board
@@ -200,149 +398,13 @@ if __name__ == '__main__':
     GameManager(TTT,players).run_game()
 
     TTT.display_board()
-
-
-
-
+    """
+    
+    
+"""----------------------------------------------------------------------------------------------"""
+    
 
 """
-
-
-class newGame():
-
-
-    def __init__(self):
-        self.board = [[0,0,0],
-                      [0,0,0],
-                      [0,0,0]]
-        self.finish = False
-
-        #print(self.board[1])
-
-    def displayBoard(self):
-        print(str(self.board[0]) + '\n' + str(self.board[1]) + '\n' + str(self.board[2]))
-
-    def play(self,p1, p2):
-        roundNum = 0
-
-        while self.finish==False:
-
-            #display the board every round
-            game.displayBoard()
-
-            #figure out which player is up to move
-            if roundNum % 2 == 0:
-                player = p1
-            else:
-                player = p2
-
-            #Ask them add the move to board
-            row, col = askForMove(player)
-            player.move(row, col)
-
-            #Check if the game is over
-            self.finish = checkIsOver(roundNum)
-            roundNum += 1
-
-
-
-
-class player():
-    def __init__(self,name):
-        self.name = name
-
-    def move(self,xpos,ypos):
-        #print(game.board[xpos][ypos])
-        game.board[xpos][ypos] = self.name
-
-def askForMove(player):
-    validInputs = False
-
-
-    while validInputs == False:
-        row = input(player.name + ", input the ROW you want to play in as an integer (0-2): ")
-
-        if row in ("0","1","2"):
-            row = int(row)
-            col = input(player.name + ", input COLUMN you want to play in as an integer (0-2): ")
-
-            if col in ("0", "1", "2"):
-                col = int(col)
-
-                if type(game.board[row][col]) == int:
-                    validInputs = True
-
-                else:
-                    print(game.board[row][col] + " already played there. Try again.")
-            else:
-                print("invalid COLUMN input. Try again.")
-        else:
-            print("invalid ROW input. Try again.")
-
-    return row, col
-
-
-def checkIsOver(roundNum):
-
-
-    
-    For reference. These are all the winning x,y coordinates.
-
-    [[[0,0],[1,0],[2,0]], #Vertical
-    [[0,1],[1,1],[2,1]], #Vertical
-    [[0,2],[1,2],[2,2]], #Vertical
-    [[0,0],[0,1],[0,2]], #Horizontal
-    [[1,0],[1,1],[1,2]], #Horizontal
-    [[2,0],[2,1],[2,2]], #Horizontal
-    [[0,0],[1,1],[2,2]], #Diagonal
-    [[0,2],[1,1],[2,0]]] #Diagonal
-    
-
-    #Splits the winning combinations up into row and col values to use in a for loop
-
-    #First lookupValue
-    winRow1 = [0,0,0,0,1,2,0,0]
-    winCol1 = [0,1,2,0,0,0,0,2]
-
-    #Second lookupValue
-    winRow2 = [1,1,1,0,1,2,1,1]
-    winCol2 = [0,1,2,1,1,1,1,1]
-
-    #Third lookupValue
-    winRow3 = [2,2,2,0,1,2,2,2]
-    winCol3 = [0,1,2,2,2,2,2,0]
-
-
-    if roundNum == 8:
-        game.displayBoard()
-        print("Cats game")
-        return True
-
-
-
-    for i in range(0,len(winRow1)):
-
-        lookupValue1 = game.board[winRow1[i]][winCol1[i]]
-        lookupValue2 = game.board[winRow2[i]][winCol2[i]]
-        lookupValue3 = game.board[winRow3[i]][winCol3[i]]
-        #print(lookupValue1,lookupValue2,lookupValue3)
-
-        if type(lookupValue1) == str and lookupValue1 == lookupValue2 == lookupValue3:
-            game.displayBoard()
-            print(lookupValue1 + " wins")
-            return True
-
-    return False
-
-
-
-
-if __name__ == '__main__':
-
-    game = newGame()
-    game.play(p1= player("Cody") , p2= player("Troy"))
-    
-    """
 import abc
 class GameInterfaceExample(abc.ABC):
     '''
@@ -431,4 +493,4 @@ def play_turn(self, player_list):
             raise RuntimeError("Invalid Move")
         self.__current_game.make_move(player, move)
         if self.__currentGame.is_game_over():
-            return
+            return"""
