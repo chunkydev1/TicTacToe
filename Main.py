0b17a2812284d8723d270d72c0b70810d580c2f6
Cody Hughes <hughes.h.cody@gmail.com>
2024-12-04 20:14:13 -0500

    This is so incomplete, but getting there. I am scared something will go wrong and I'll lose it all so this is a checkpoint save. Real commit coming soon.

--- Main.py
+++ Main.py
@@ -16,44 +16,58 @@ class Gameboard(abc.ABC):
         pass
 
     @abc.abstractmethod
+    def get_input_rules(self):
+        pass
+
+    @abc.abstractmethod
+    def get_win_criteria(self):
+        pass
+
+    @abc.abstractmethod
     def display_board(self) -> None:
         pass
+
+
+
+class InputRules(abc.ABC):
+
+    def __init__(self,inputs: list[str]) -> None:
+        self.__vinputs = inputs
 
     @abc.abstractmethod
     def valid_inputs(self,playerinputs: list[str]) -> bool:
         pass
 
+    #spot_is_playable could be in gameboard because its sole responsibility is to look at the board, but I think its better here because the rules of a valid input are 2 things:
+    # 1- must be within bounds (def valid_inputs)
+    # 2- must be a location not already played (spot_is_playable).
     @abc.abstractmethod
-    def spot_is_playable(self,playerinputs: list[str]) -> bool:
+    def spot_is_playable(self,board: list[list[str]], playerinputs: list[str]) -> bool:
         pass
 
-    #unused
-    @abc.abstractmethod
     def get_valid_inputs(self) -> list[str]:
-        pass
+        return self.__vinputs
 
-    @abc.abstractmethod
-    def game_is_won(self,name: str) -> bool:
-        pass
 
-    @abc.abstractmethod
-    def check_pattern(self, length: int) -> tuple[bool, None or str]:
-        pass
-
-    @abc.abstractmethod
-    def search_board(self, row: int, col: int, xdir: int, ydir: int, length: int) -> bool:
-        pass
-
-    @abc.abstractmethod
-    def game_is_tie(self) -> bool:
-        pass
-
-class InputRules(abc.ABC):
-    pass
 
 
 class WinCriteria(abc.ABC):
-    pass
+
+    @abc.abstractmethod
+    def game_is_won(self, board: list[list[str]]) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def check_pattern(self, board: list[str]) -> tuple[bool, None or str]:
+        pass
+
+    @abc.abstractmethod
+    def search_board(self, board: list[list[str]], row: int, col: int, xdir: int, ydir: int, length: int, valuetolookup: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def game_is_tie(self, board: list[list[str]]) -> bool:
+        pass
 
 
 
@@ -87,7 +101,9 @@ class tictactoe(Gameboard):
         self.__validinputs = ["0","1","2"]
         self.__winlength = 3
 
-# board info -------------------------------------------------------
+        self.__rules = tttinputrules(self.__validinputs)
+        self.__win = tttwin(self.__winlength)
+
     def set_move_on_board(self,playermove: list[str],name: str) -> None:
         x = int(playermove[0])
         y = int(playermove[1])
@@ -97,11 +113,28 @@ class tictactoe(Gameboard):
     def get_board(self) -> list[list[str]]:
         return self.__board
 
+    def get_input_rules(self) -> InputRules:
+        return self.__rules
+
+    def get_win_criteria(self) -> WinCriteria:
+        return self.__win
+
     def display_board(self) -> None:
         for row in self.__board:
             print(row)
 
-# input rules -------------------------------------------------------
+
+
+
+
+
+
+
+
+class tttinputrules(InputRules):
+
+    def __init__(self,inputs) -> None:
+        self.__validinputs = inputs
 
     def valid_inputs(self,playerinputs: list[str]) -> bool:
         if len(playerinputs) != 2:
@@ -114,11 +147,11 @@ class tictactoe(Gameboard):
         #print("valid")
         return True
 
-    def spot_is_playable(self,playerinputs: list[str]) -> bool:
+    def spot_is_playable(self, board: list[list[str]], playerinputs: list[str]) -> bool:
         x = int(playerinputs[0])
         y = int(playerinputs[1])
 
-        if self.__board[x][y] != "":
+        if board[x][y] != "":
             #print("not playable")
             return False
         #print("playable")
@@ -128,19 +161,31 @@ class tictactoe(Gameboard):
     def get_valid_inputs(self) -> list[str]:
         return self.__validinputs
 
-# Win criteria -------------------------------------------------------
-    def game_is_won(self,playername: str) -> bool:
-        found, winner = self.check_pattern(self.__winlength)
+
+
+
+
+
+
+
+
+class tttwin(WinCriteria):
+
+    def __init__(self,winlength: int) -> None:
+        self.__winlength = winlength
+
+
+    def game_is_won(self,board: list[list[str]]) -> bool:
+        found, winner = self.check_pattern(board)
         if found:
-            self.display_board()
             print("Winner: " + winner)
             return True
 
 
-    def check_pattern(self, length: int) -> tuple[bool, None or str]:
+    def check_pattern(self, board: list[list[str]]) -> tuple[bool, None or str]:
 
-        rows = len(self.__board)
-        cols = len(self.__board[0])
+        rows = len(board)
+        cols = len(board[0])
 
         #             right,   down, right+down, left+down
         directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
@@ -148,36 +193,37 @@ class tictactoe(Gameboard):
         for row in range(rows):
             for col in range(cols):
                 for xdir, ydir in directions:
-                    if self.search_board(row, col, xdir, ydir, length):
-                        return True, self.__board[row][col]
+                    valuetolookup = board[row][col]
+                    if self.search_board(board, row, col, xdir, ydir, self.__winlength, valuetolookup):
+                        return True, board[row][col]
 
         return False, None
 
 
-    def search_board(self, row: int, col: int, xdir: int, ydir: int, length: int) -> bool:
+    def search_board(self, board: list[list[str]], row: int, col: int, xdir: int, ydir: int, length: int, valuetolookup: str) -> bool:
 
-        lookupvalue = self.__board[row][col]
-        if lookupvalue == "":
+        if valuetolookup == "":
             return False
 
         for i in range(length):
             r = row + i * xdir
             c = col + i * ydir
             # Check bounds and value
-            if not (0 <= r < len(self.__board) and 0 <= c < len(self.__board[0]) and self.__board[r][c] == lookupvalue):
+            if not (0 <= r < len(board) and 0 <= c < len(board[0]) and board[r][c] == valuetolookup):
                 return False
 
         return True
 
 
-    def game_is_tie(self) -> bool:
-        for row in self.__board:
+    def game_is_tie(self, board: list[str]) -> bool:
+        for row in board:
             if "" in row:
                 return False
 
-        self.display_board()
         print("game is a tie")
         return True
+
+
 
 
 
@@ -193,11 +239,15 @@ class manualplayer(players):
         return re.findall(r'[0-9]+',pinput)
 
 
+
+
+
+
     #Unsure where this game_in_progress should go.
     #Part of me wants it in the game object because the game is responsible for doing game things (the game flow)....
     #BUT the game doesn't know anything about a player object so I know this isn't correct.
 
-def game_in_progress(g: Gameboard, groupofplayers: list[players]) -> bool:
+"""def game_in_progress(g: Gameboard, groupofplayers: list[players]) -> bool:
     for p in groupofplayers:
         g.display_board()
         pname = p.get_name()
@@ -209,12 +259,19 @@ def game_in_progress(g: Gameboard, groupofplayers: list[players]) -> bool:
         g.set_move_on_board(move,pname)
         if g.game_is_won(pname) or g.game_is_tie():
             return False
-    return True
+    return True"""
+
+
+
 
 def RandomName():
     name = names.get_first_name()
     print(name)
     return name
+
+
+
+
 
 
 class playerAI(players):
@@ -224,7 +281,7 @@ class playerAI(players):
         self.__name = self.get_name()
 
     def get_move(self) -> list[str]:
-        x = str(random.randint(0,2))
+        x = str(random.randint(0, 2))
         y = str(random.randint(0, 2))
         print(self.__name + " has made a move")
         return [x,y]
@@ -232,13 +289,73 @@ class playerAI(players):
 
 if __name__ == '__main__':
     AI = playerAI()
-
     ttt = tictactoe()
     playersingame = [manualplayer(), AI]
 
-    #please don't judge me too hard for this. I am unsure how to nicely facilitate the game.
-    while game_in_progress(ttt, playersingame):
-        pass
+    ttt.display_board()
+    for p in playersingame:
+        board = ttt.get_board()
+        move = p.get_move()
+        name = p.get_name()
+
+        print(move)
+        gameinputs = ttt.get_input_rules()
+        if not gameinputs.valid_inputs(move) or not gameinputs.spot_is_playable(board, move):
+            print("NValid")
+        else:
+            print("Valid")
+            ttt.set_move_on_board(move,name)
+
+        winrules = ttt.get_win_criteria()
+        if winrules.game_is_won(board) or winrules.game_is_tie(board):
+            print("Winner" + name)
+        else:
+            print("No Winner")
+
+        ttt.display_board()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
